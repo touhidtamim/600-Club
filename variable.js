7b0a541462db53acc6523b53be159b5d65f8990d
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-01 10:05:06 +0600

    solved variable problems

---
+++ variable.js
@@ -0,0 +1,16 @@
+// Variable problems without console
+
+const birthYear = 2005;
+
+const countSiblings = 2;
+
+const students = 58;
+
+const marks = 76;
+
+const rooms = 6;
+
+const dailyStudyHour = 8;
+
+let phone = 1774130037;
+const total = 1 + 7 + 7 + 4 + 1 + 3 + 0 + 0 + 3 + 7;
