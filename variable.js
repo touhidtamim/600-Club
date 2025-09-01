ccf630d16644499296a69628f64aaac54e21f4a0
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-01 10:18:10 +0600

    more varable problems solved

--- variable.js
+++ variable.js
@@ -14,3 +14,27 @@ const dailyStudyHour = 8;
 
 let phone = 1774130037;
 const total = 1 + 7 + 7 + 4 + 1 + 3 + 0 + 0 + 3 + 7;
+
+// Variable problems with console
+
+const myAge = 21;
+console.log(myAge);
+
+const booksToRead = 7;
+console.log(booksToRead);
+
+let wakeUpTime = 8;
+wakeUpTime = 6;
+console.log(wakeUpTime);
+
+const wastedTime = 8;
+console.log(wastedTime);
+
+const borrow = 0;
+console.log(borrow);
+
+const goals = 5;
+console.log(goals);
+
+const pillow = 3;
+console.log(pillow);
