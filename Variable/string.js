e4079699db996a9f07b35c3f78c596628c06245a
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-01 20:00:34 +0600

    Added Variable folder with files

---
+++ Variable/string.js
@@ -0,0 +1,22 @@
+// String Problems
+
+const name = "Tamim";
+console.log(name);
+
+const address = "Gaibandha";
+console.log(address);
+
+const friendsUniversity = "RUET";
+console.log(friendsUniversity);
+
+const fan = "nai keu";
+console.log(fan);
+
+const movieOfTheYear = "The Art Of Racing in The Rain";
+console.log(movieOfTheYear);
+
+const hobby = "World Tour";
+console.log(hobby);
+
+const color = "White";
+console.log(color);
