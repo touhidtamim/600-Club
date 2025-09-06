d9c2f5891628a046801cb3a393f31881495caf60
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-06 07:53:03 +0600

    solved data type checking problem

---
+++ DataType/checkDataType.js
@@ -0,0 +1,23 @@
+// 1
+const price = 555;
+console.log(typeof price);
+
+// 2
+const learning = true;
+console.log(typeof learning);
+
+// 3
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+// 4
+let temperature = 102.5;
+console.log(typeof temperature);
+
+// 5
+let isHungry = true;
+console.log(typeof isHungry);
+
+// 6
+let isRich = true;
+console.log(typeof isRich);
