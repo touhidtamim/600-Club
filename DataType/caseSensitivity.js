238940934388154af0c65a0b9195ac59fca9a878
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-06 08:44:15 +0600

    solved case sensitivity and reserved kayword error problems

---
+++ DataType/caseSensitivity.js
@@ -0,0 +1,26 @@
+// 1
+let myFavouriteFood = "Beef";
+
+// 2
+const city = "chittagong";
+const City = "Dhaka";
+console.log(city, City);
+
+// 3
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName, username);
+
+// 4
+let myHomeAddress = "Dhaka";
+
+// 5
+// let super = 17; thik nai reserved keyword,
+
+// 6
+// Reserved keyword
+// let class = 12;
+
+// 7
+/*  variable er namer suru songkha diye korle error dey but name suru bad diye onno jaygay (middle/ seshe) use kora jay */
+// let 2ndPosition = "Shachin Tendulkar"
