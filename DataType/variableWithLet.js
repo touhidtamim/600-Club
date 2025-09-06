2692081b18f8e1038a92be5a9422311268b71f97
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-06 08:09:40 +0600

    solved variable declaration and value changing problem

---
+++ DataType/variableWithLet.js
@@ -0,0 +1,36 @@
+// 1
+let age = 21;
+console.log(age);
+age = 22;
+console.log(age);
+
+// 2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+// 3
+let studyTime = 4;
+studyTime = 7;
+console.log(studyTime);
+
+// 4
+let friendsCount = 2;
+friendsCount = 0;
+console.log(friendsCount);
+
+// 5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+// 6
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
+// 7
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
