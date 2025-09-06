f3b9a5a352c459f082e9837fb0713fa6510b695b
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-06 08:27:46 +0600

    solved making Comments problem

---
+++ DataType/makingComment.js
@@ -0,0 +1,35 @@
+// 1
+// Target runs set for the opponent team
+let score = 87;
+
+// 2
+// The radius of the circle used in our calculation
+// This value will determine the size of the circle
+// Make sure to update it if the circle changes
+let radius = 20;
+
+// 3
+/*Number of my favourite dresses.*/
+let dress = 5;
+
+// 4
+// Amount of gift money given for Eid.
+let giftMoney = 500;
+
+// 5
+// Total study time in hours for today
+// 2 hours for Math
+// 1.5 hours for English
+// 2 hours for Programming
+// 1.5 hours for problem Solving
+let studyTime = 7;
+
+// 6
+// Total amount to send: 1000 Tk
+// Send charge: 20 Tk
+// Remaining 980 Tk will be received by the person
+const sendMoney = 1000;
+
+// 7
+// Product price 1430 + delivery charge 120
+const pay = 1550;
