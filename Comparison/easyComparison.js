209fb3b29a4a0f91a6d094115f5a0134312788cd
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-07 10:22:20 +0600

    solved easy comparison problems

---
+++ Comparison/easyComparison.js
@@ -0,0 +1,41 @@
+//1
+let iEat = 7;
+let brotherEats = 5;
+
+console.log(iEat > brotherEats); //output: true
+
+//2
+let class1 = 45;
+let class2 = 50;
+
+console.log(class1 < class2); //output: true
+
+//3
+let iGot = 75;
+let myFriendGot = 75;
+
+console.log(iGot == myFriendGot); //output: true
+
+//4
+const target = 100;
+let accumulate = 95;
+
+console.log(target == accumulate); //output: false
+
+//5
+let num1 = 10;
+let num2 = 7;
+
+console.log(num1 == num2); //output: false
+
+//6
+let num3 = 20;
+let num4 = 15;
+
+console.log(num3 <= num4); //output: false
+
+// 7
+let IGot = 45;
+let FriendGot = 97;
+
+console.log(FriendGot < IGot); //output: false
