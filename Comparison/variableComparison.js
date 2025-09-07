589608f55c0c10dfdcc628438947d83cb0e39aa3
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-07 10:36:26 +0600

    solved simple variable comparison

---
+++ Comparison/variableComparison.js
@@ -0,0 +1,46 @@
+//1
+let brotherAge = 10;
+let myAge = 15;
+
+console.log(brotherAge <= myAge); //output: true
+
+//2
+const passMark = 33;
+let iGot = 45;
+
+console.log(iGot > passMark); //output: true
+
+//3
+let jamburaPrice = 60;
+let kodbelPrice = 50;
+
+console.log(jamburaPrice > kodbelPrice); //output: true
+
+//4
+let iRead = 10;
+let myFriendReads = 10;
+
+console.log(iRead == myFriendReads); //output: true
+
+//5
+let myBiscuits = 15;
+let myFriendsBiscuits = 20;
+
+console.log(myBiscuits < myFriendsBiscuits); //output: true
+
+//6
+let realMadrid = 2;
+let Barca = 3;
+console.log(realMadrid < Barca); //output: true
+
+//7
+const name = "Rahim";
+const name2 = "Karim";
+
+console.log(name === name2); //output: false
+
+//8
+let IReadToday = true;
+let myFriendReadsToday = false;
+
+console.log(IReadToday === myFriendReadsToday); //output: false
