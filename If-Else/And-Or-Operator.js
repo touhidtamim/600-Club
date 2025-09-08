1960265febf515be8b61aed5c9d4af28339e5453
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-08 09:34:30 +0600

    solved problems with And-Or operator

---
+++ If-Else/And-Or-Operator.js
@@ -0,0 +1,87 @@
+//1
+let age = 17;
+const height = 60;
+
+if (age > 18 && height > 60) {
+  console.log("gari thelbe");
+} else {
+  console.log("garite bose thakbe");
+}
+
+//2
+const mathScore = 70;
+const englishScore = 86;
+
+if (mathScore > 80 || englishScore > 85) {
+  console.log("approved for admission");
+} else {
+  console.log("biye diye dibe");
+}
+
+//3
+const gpa = 5;
+const monthlyFamilyIncome = 10000;
+
+if (gpa === 5 && monthlyFamilyIncome < 10000) {
+  console.log("scholarship deya hobe");
+} else {
+  console.log("nijer takay porte hobe");
+}
+
+//4
+const Age = 29;
+const experience = 2;
+
+if (Age < 30 && experience > 2) {
+  console.log("exam dite parbe");
+} else {
+  console.log("exam a boste parbe na");
+}
+
+//5
+const eggInFridge = 15;
+const chickenInFridge = true;
+
+if (eggInFridge > 12 || chickenInFridge !== true) {
+  console.log("dimer korma ranna hobe");
+} else {
+  console.log("ruti kola kheye thakte hobe");
+}
+
+//6
+const temperature = 97;
+const cough = true;
+
+if (temperature > 100 || cough === true) {
+  console.log("Doctor dekhate hobe");
+} else {
+  console.log("ghumao");
+}
+
+//7
+const attendance = 85;
+const homeworkSubmitted = true;
+
+if (attendance > 80 && homeworkSubmitted === true) {
+  console.log("Exam Attend korte parbe");
+} else {
+  console.log("Auto Fail");
+}
+
+//8
+const isCurrentAvailable = true;
+const isMobileCharged = true;
+
+if (isCurrentAvailable === false && isMobileCharged === flase) {
+  console.log("porte bosbo");
+} else {
+  console.log("Video game khelbo");
+}
+
+//9
+const shirtPrice = 1220;
+const isCouponAvailable = true;
+
+if (shirtPrice > 1000 && isCouponAvailable === true) {
+  console.log("deshnetake 20% discount ditei hobe");
+}
