67f1d8e44e74b23fc0ea239aadf2d41020dbec49
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-08 09:02:56 +0600

    solved simple if else problems

---
+++ If-Else/simpleIfElse.js
@@ -0,0 +1,53 @@
+//1
+const returnHomeAt = 5;
+
+if (returnHomeAt <= 6) {
+  console.log("ammu nasta khaoabe");
+} else {
+  console.log("jharu diye marbe");
+}
+
+//2
+const isLogin = true;
+
+if (isLogin === true) {
+  console.log("welcome");
+} else {
+  console.log("Get Lost!");
+}
+
+//3
+const runTime = 5;
+
+if (runTime === 5) {
+  console.log("chocolet pabo");
+} else {
+  console.log("mota vuri hobe");
+}
+
+//4
+const GotInExam = 75;
+
+if (GotInExam > 80) {
+  console.log("abba bike kine dibe");
+} else {
+  console.log("kopale bike nai");
+}
+
+//5
+const showTime = 8;
+
+if (showTime < 9) {
+  console.log("ami movie dekhbo");
+} else {
+  console.log("nake tel diye ghumabo");
+}
+
+//6
+const temperature = 29;
+
+if (temperature >= 30) {
+  console.log("air condition on korbo");
+} else {
+  console.log("kombol gaye diye ghumabo");
+}
