8f41b00615fba6fe29781b060c664cd94abd7d52
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-15 07:23:27 +0600

    solved ternery operator problems

---
+++ If-Else/ternery.js
@@ -0,0 +1,40 @@
+//1
+let customerBill = 3500;
+
+let cashback = customerBill > 3000 ? 500 : 0;
+console.log(cashback);
+
+//2
+let age = 20;
+
+age > 15 ? console.log("Teenager") : console.log("Child");
+
+//3
+let isLoggedIn = true;
+
+let message = isLoggedIn ? "welcome back" : "Please Login";
+console.log(message);
+
+//4
+let oilInCar = "full";
+
+oilInCar = "full"
+  ? console.log("Ready for a long drive")
+  : console.log("Fill the tank");
+
+//5
+let isPassedInExam = true;
+
+isPassedInExam
+  ? console.log("Party Time")
+  : console.log("Next semister a serious study korbo");
+
+//6
+let sunny = true;
+
+sunny ? console.log("Go outside, enjoy the sun") : console.log("Stay Home");
+
+//7
+let Expensive = false;
+
+console.log(Expensive === false ? "I will buy this item" : "tk nai pore kinbo");
