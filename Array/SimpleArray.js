59cfaa91f17d9ba3e88a9b97ef2f5e137267a71e
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-17 09:38:31 +0600

    simple array making problem solved

---
+++ Array/SimpleArray.js
@@ -0,0 +1,67 @@
+//1
+let num = [71, 71, 73, 74, 75, 76, 77, 78, 79];
+
+//2
+let insideFridge = ["apple", " Banana", "Orange", "Mango", "Guava"];
+
+//3
+let buyFromBazar = [
+  "potato",
+  "onion",
+  "Brinjal",
+  "Radish",
+  "carrot",
+  "tomato",
+  "ladies finger",
+  "gourd",
+  "Cucumber",
+  "Cataract",
+];
+
+console.log(buyFromBazar.length);
+
+//4
+let favMovie = [
+  "October Sky",
+  "A Beautiful Mind",
+  "Good Will Hunting",
+  "The Shawshank Redemption",
+  "Schindler's List",
+];
+
+//5
+let oddNum = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+//6
+const colorInsideMe = [
+  "red",
+  "blue",
+  "green",
+  " yellow",
+  " orange",
+  "purple",
+  " pink",
+  "black",
+  "white",
+  "brown",
+  "gray",
+];
+
+console.log(colorInsideMe.length);
+
+//7
+const capital = [
+  "Dhaka",
+  "Beijing",
+  " Kinshasa",
+  "New Delhi",
+  "Tokyo",
+  "Moscow",
+  " Jakarta",
+  "Bangkok",
+  " Cairo",
+  "Seoul",
+  " London",
+];
+
+console.log(capital.length);
