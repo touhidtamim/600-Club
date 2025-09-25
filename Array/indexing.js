71629df4ff29b1e51a0c7609931033e00c82f29f
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-25 21:45:59 +0600

    solved simple indexing problem

---
+++ Array/indexing.js
@@ -0,0 +1,45 @@
+//1
+const friends = ["Adnan", "Limon", "Shiam", "Shakib", "Tamim"];
+console.log(friends[3]);
+
+//2
+const books = [
+  "Atomic Habits",
+  "Deep Work",
+  "The 7 Habits of Highly Effective People",
+  "Eat That Frog!",
+  "Getting Things Done",
+  "The ONE Thing",
+  "The 5 AM Club",
+];
+console.log(books[4]);
+
+//3
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+num[6] = 30;
+console.log(num[6]);
+
+//4
+let game = ["PUBG", "Free Fire", "Candy Crash", "Temple Run"];
+game[2] = "Subway Surfer";
+console.log(game[2]);
+
+//5
+let familyMembers = ["Tamim", "Tuba", "Tahmina", "Zonayed"];
+console.log(familyMembers[2]);
+
+//6
+const countries = [
+  "Japan",
+  "Switzerland",
+  "Italy",
+  "Thailand",
+  "Turkey",
+  "Australia",
+  "Canada",
+];
+console.log(countries[3]);
+
+//7
+let things = ["books", "pen", "Laptop", "Clock"];
+console.log(things[6]);
