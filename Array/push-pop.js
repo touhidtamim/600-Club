110f5d0fe5b6d9c9d2819993f4b674e057c608dd
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-26 21:17:16 +0600

    solved push , pop , shift , unshift related problems

---
+++ Array/push-pop.js
@@ -0,0 +1,39 @@
+//1
+let num = [10, 20, 30, 40, 50];
+num.push(60);
+console.log(num);
+
+//2
+let names = ["sojib", "shagor", "shakib", "Shohel"];
+names.push("sumon");
+console.log(names);
+
+//3
+let games = [
+  "Roblox",
+  "Candy Crush Saga",
+  "Among Us",
+  "Free Fire",
+  "PUBG",
+  "Among Us",
+];
+games.pop();
+console.log(games);
+
+//4
+let arr = [24, 36, 48, 60];
+arr.unshift(12);
+console.log(arr);
+
+//5
+let books = [
+  "Atomic Habits",
+  "Deep Work",
+  "The 7 Habits of Highly Effective People",
+  "Eat That Frog!",
+  "Getting Things Done",
+  "The ONE Thing",
+  "The 5 AM Club",
+];
+books.shift();
+console.log(books);
