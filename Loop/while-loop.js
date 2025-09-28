116decdb0972a6a1bd8aae9078a11b2b4d77d494
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-28 23:23:45 +0600

    solved while loop related problems

---
+++ Loop/while-loop.js
@@ -0,0 +1,34 @@
+//1
+let num = 0;
+while (num < 10) {
+  console.log("Ami daily min 3 hour coding practice korbo");
+  num++;
+}
+
+//2
+let number = 1;
+while (number < 21) {
+  console.log(number);
+  number++;
+}
+
+//3
+let minNumber = 50;
+while (minNumber < 101) {
+  console.log(minNumber);
+  minNumber++;
+}
+
+//4
+let numbers = 1;
+while (numbers <= 10) {
+  console.log(numbers * 3);
+  numbers++;
+}
+
+//5
+let Numbers = 11;
+while (Numbers <= 110) {
+  console.log(Numbers / 2);
+  Numbers++;
+}
