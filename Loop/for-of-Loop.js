c4b99b8e4feb67383c7a0268741bfa2171cc1b26
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-28 19:03:22 +0600

    solved for of loop problems

---
+++ Loop/for-of-Loop.js
@@ -0,0 +1,41 @@
+//1
+let subjects = ["Bangla", "English", "Physics", "Math", "Biology"];
+for (const subject of subjects) {
+  console.log(subject);
+}
+
+//2
+let foods = ["Noodles", "Pasta", "Milk", "Apple", "Guava"];
+for (const food of foods) {
+  console.log(food);
+}
+
+//3
+let years = [1982, 1985, 2002, 2005, 2012];
+for (const year of years) {
+  console.log(year);
+}
+
+//4
+let players = [
+  "Sachin Tendulkar",
+  "Rohit Sharma",
+  "Virat Kohli",
+  "AB de Villiers",
+  "Ben Stokes",
+  "MS Dhoni",
+  "Jacques Kallis",
+  "Wasim Akram",
+  "Shane Warne",
+  "Mitchell Starc",
+  "Muttiah Muralitharan",
+];
+for (const player of players) {
+  console.log(player);
+}
+
+//5
+let examDates = ["17-11-25", "20-11-25", "22-11-25", "27-11-25", "29-11-25"];
+for (const examDate of examDates) {
+  console.log(examDate);
+}
