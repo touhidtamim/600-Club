d4e36c942f040765b2fe23a0415e4b3bdbf70b35
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-28 23:47:10 +0600

    solved sum realted problems using while loop

---
+++ Loop/sumWith-while-loop.js
@@ -0,0 +1,46 @@
+//1
+let num1 = 50;
+while (num1 <= 100) {
+  console.log(num1);
+  num1++;
+}
+
+//2
+let num2 = 5;
+let sum2 = 0;
+
+while (num2 <= 15) {
+  sum2 = sum2 + num2;
+  num2++;
+}
+console.log(sum2);
+
+//3
+let roll = 1;
+let sumOfRoll = 0;
+
+while (roll <= 50) {
+  sumOfRoll = sumOfRoll + roll;
+  roll++;
+}
+console.log(sumOfRoll);
+
+//4
+let num4 = 21;
+let sum4 = 0;
+
+while (num4 <= 50) {
+  sum4 = sum4 + num4;
+  console.log(sum4);
+  num4++;
+}
+
+//5
+let num5 = 20;
+let sum5 = 0;
+
+while (num5 <= 40) {
+  sum5 = sum5 + num5;
+  num5++;
+}
+console.log(sum5);
