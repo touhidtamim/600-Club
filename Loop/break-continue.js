55e137848bff69a3ef8f91517ebf51c211bd8321
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-29 00:35:53 +0600

    solved code break & skip realted problems using for loop

---
+++ Loop/break-continue.js
@@ -0,0 +1,47 @@
+//1
+for (i = 1; i <= 30; i++) {
+  if (i === 16) {
+    break;
+  }
+  console.log(i);
+}
+
+//2
+for (i = 1; i <= 40; i++) {
+  if (i % 7 === 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+//3
+for (i = 1; i <= 15; i++) {
+  if (i === 9) {
+    continue;
+  }
+  console.log(i);
+}
+
+//4
+for (i = 1; i <= 20; i++) {
+  if (i === 12) {
+    continue;
+  }
+  console.log(i);
+}
+
+//5
+for (i = 1; i <= 25; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+//6
+for (i = 91; i <= 120; i++) {
+  if (i % 10 === 0) {
+    continue;
+  }
+  console.log(i);
+}
