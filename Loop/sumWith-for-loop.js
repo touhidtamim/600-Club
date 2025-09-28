6a37da71b6d79959f0501acd341ad52f2f798c51
Touhid Tamim <touhidtamim1@gmail.com>
2025-09-28 23:59:07 +0600

    solved sum realted problems using for loop

---
+++ Loop/sumWith-for-loop.js
@@ -0,0 +1,18 @@
+//1
+for (let i = 150; i <= 170; i++) {
+  console.log(i);
+}
+
+//2
+let sum2 = 0;
+for (let roll = 31; roll <= 58; roll++) {
+  sum2 = sum2 + roll;
+}
+console.log(sum2);
+
+//3
+let sum3 = 0;
+for (let roll3 = 25; roll3 <= 75; roll3++) {
+  sum3 = sum3 + roll3;
+}
+console.log(sum3);
