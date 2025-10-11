642ac8f8240184bc2ade7d8e1d98443fd7ed6633
Touhid Tamim <touhidtamim1@gmail.com>
2025-10-11 18:58:40 +0600

    solved 2 problems

--- Function/complex-problem.js
+++ Function/complex-problem.js
@@ -1,1 +1,19 @@
 //1
+function checkArrLength(arr) {
+  if (arr.length % 2 === 0) {
+    console.log("array length is even");
+  } else {
+    console.log("array length is odd");
+  }
+}
+
+let fruits = ["apple", "mango", "lichi", "banana"];
+checkArrLength(fruits);
+
+//2
+function returnFirstLetter(name) {
+  return name[0];
+}
+
+console.log(returnFirstLetter("Raju"));
+console.log(returnFirstLetter("Tamim"));
