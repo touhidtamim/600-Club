151d36a29537968c7c4b9a80659185e790e70bdc
Touhid Tamim <touhidtamim1@gmail.com>
2025-10-18 23:32:06 +0600

    solved problem 1

--- Function/Advanced-Problems.js
+++ Function/Advanced-Problems.js
@@ -1,4 +1,17 @@
 //1
-function sumOfOddNumber(arr) {}
+function sumOfOddNumbers(arr) {
+  let sum = 0;
+
+  for (let num of arr) {
+    if (num % 2 !== 0) {
+      sum += num;
+    }
+  }
+
+  return sum;
+}
+
+const numbers = [5, 15, 8, 7];
+console.log(sumOfOddNumbers(numbers));
 
 //2
