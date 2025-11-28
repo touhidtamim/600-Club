eda914b32ca76edcb4e3099c511235435e096cc2
Touhid Tamim <touhidtamim1@gmail.com>
2025-11-28 21:11:30 +0600

    solved first problem of complex logic building

---
+++ Logic_Build_Up/Complex-Mathematical-Logic-part2.js
@@ -0,0 +1,20 @@
+//1
+function avgOfEven(arr) {
+  let sum = 0;
+  let totalEven = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sum = sum + arr[i];
+      totalEven = totalEven + 1;
+    }
+  }
+
+  let avg = sum / totalEven;
+  return avg;
+}
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let result = avgOfEven(nums);
+console.log(result);
